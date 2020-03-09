25c67d121d10bec53273da2a4513d186b33af9c0
Zvika Glick <zvika.glick@kaltura.com>
2020-03-09 17:48:26 +0200

    add container

---
+++ docker/container/main.tf
@@ -0,0 +1,10 @@
+#Start the container
+
+resource "docker_container" "container_id" {
+  image = "${var.image}"
+  name =  "${var.container_name}"
+  ports {
+    internal = "${var.int_port}"
+    external = "${var.ext_port}"
+  }
+}
---
+++ docker/container/outputs.tf
@@ -0,0 +1,9 @@
+#outputs the IP address and name of the container
+
+output "ip" {
+  value = "${docker_container.container_id.ip_address}"
+}
+
+output "container_name" {
+  value = "${docker_container.container_id.name}"
+}
